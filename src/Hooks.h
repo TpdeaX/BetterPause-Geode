323e25a1a85f9e4d417e6df1a90ac971c85bee52
TpdeaX <josephmd10@gmail.com>
2024-03-11 19:10:36 -0500

    no mac :c

--- src/Hooks.h
+++ src/Hooks.h
@@ -224,7 +224,6 @@ class $modify(PauseLayer) {
 		m_fields->hasPosibleExitHotKey = false;
 	}
 
-#ifdef GEODE_IS_ANDROID
 	void keyBackClicked() {
 		if (Mod::get()->getSettingValue<bool>("remove-exit-back-button")) {
 			m_fields->hasPosibleExitHotKey = true;
@@ -234,7 +233,6 @@ class $modify(PauseLayer) {
 
 		m_fields->hasPosibleExitHotKey = false;
 	}
-#endif
 
 #ifdef GEODE_IS_MACOS
 	void onQuit(cocos2d::CCObject* sender) {
